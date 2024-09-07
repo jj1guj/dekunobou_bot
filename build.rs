fa1a15728daaba13947d777419027dcdb0417809
jj1guj <jj1guj@jj1guj.net>
2024-09-08 01:34:07 +0900

    dekunobouにおいてfor_rustブランチの代わりにmainブランチを参照するようにした

--- build.rs
+++ build.rs
@@ -2,11 +2,11 @@
 fn main(){
     // CMakeLists.txtが存在するディレクトリを指定します
     // プロジェクトディレクトリからの相対位置となります
-    let dst = cmake::build("dekunobou");
+    let dst = cmake::build("dekunobou/web/api/lib/dekunobou_lib/src/cpp");
     println!("cargo:rustc-link-search=native={}", dst.display());
 
     // staticライブラリとして他に利用するライブラリはなし
-    //println!("cargo:rustc-link-lib=static=");
+    println!("cargo:rustc-link-lib=gomp");
 
     // C++ソースコードの場合は必ずこれを追加すること
     println!("cargo:rustc-link-lib=dylib=stdc++");
