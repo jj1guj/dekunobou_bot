65ee72f2d1b61523d74a872cd7c3682f57c05715
jj1guj <jj1guj@jj1guj.net>
2024-09-14 00:26:55 +0900

    記法が古かった箇所を修正

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use rand::SeedableRng;
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
 use tokio::sync::Mutex;
-extern crate dekunobou;
+use dekunobou;
 
 #[derive(Serialize,Deserialize,Debug)]
 struct WSResult{
